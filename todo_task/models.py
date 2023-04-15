e2ede4191bc2bbf22a3e5b31ffd492c436de11fc
happyITsemi <65163342+HappyITSemi@users.noreply.github.com>
2023-04-15 13:22:15 +0900

    todo_task makemaigrations-1

--- todo_task/models.py
+++ todo_task/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)  # この行を追加
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
