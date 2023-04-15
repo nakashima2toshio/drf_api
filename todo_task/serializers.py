c24741851fe8a1c163dd31cba7800c4325088455
happyITsemi <65163342+HappyITSemi@users.noreply.github.com>
2023-04-15 12:58:02 +0900

    todo_task makemaigrations実施前-3

--- todo_task/serializers.py
+++ todo_task/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from todo_task.models import TodoTask
+from todo_task.models import Task
 
 class TodoTaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = TodoTask
+        model = Task
         fields = '__all__'
